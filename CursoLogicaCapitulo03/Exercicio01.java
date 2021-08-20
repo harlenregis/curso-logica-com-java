fecbf0973af744281bf09a02210906acb6f7ab87
Harlen Regis <72930966+harlenregis@users.noreply.github.com>
2021-08-20 15:19:21 -0300

    Calculando o quadrado de um número

---
+++ CursoLogicaCapitulo03/Exercicio01.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exercicio01 {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Calculando o quadrado de um número");
+		
+		System.out.print("Informe um número: ");
+		Integer numero = scanner.nextInt();
+		
+		Integer quadrado = numero * numero;
+		
+		System.out.println("O quadrado de: " + numero + " é " + quadrado);
+		
+		scanner.close();
+	}
+}
