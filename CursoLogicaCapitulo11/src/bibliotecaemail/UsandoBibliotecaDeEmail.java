25ac4bd5bebf0436040fb6c47aedab71e64ea079
Harlen Regis <72930966+harlenregis@users.noreply.github.com>
2022-03-15 16:54:28 -0300

    Utilizando uma biblioteca de terceiro para envio de e-mails

---
+++ CursoLogicaCapitulo11/src/bibliotecaemail/UsandoBibliotecaDeEmail.java
@@ -0,0 +1,25 @@
+package bibliotecaemail;
+
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
+public class UsandoBibliotecaDeEmail {
+
+	public static void main(String[] args) throws EmailException {
+		Email email = new SimpleEmail();
+		email.setHostName("smtp.gmail.com");
+		email.setSmtpPort(465);
+		email.setAuthenticator(new DefaultAuthenticator("cursologica.algaworks", "!@#$%qwert"));
+		email.setSSLOnConnect(true);
+		email.setFrom("cursologica.algaworks@gmail.com");
+		email.setSubject("TestMail");
+		email.setMsg("This is a test mail ... :-)");
+		email.addTo("harlenregis@hotmail.com");
+		email.send();
+		
+		System.out.println("Fim...");
+	}
+
+}
